823c9ff3282007c2bd102247f9dc80530c334182
zerowong <wongzero@foxmail.com>
2021-11-04 00:17:53 +0800

    fix: `button` icon is not centered

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import classNames from 'classnames'
 import { Transition } from '@headlessui/react'
-import { Icon } from './icon'
+import { LoadingIcon } from './icon'
 
 type ButtonProps = JSX.IntrinsicElements['button'] & {
   /**
@@ -29,40 +29,13 @@ type ButtonProps = JSX.IntrinsicElements['button'] & {
    */
   link?: boolean
   /**
-   * 容器类名
+   * 前置图标容器类名
    */
-  wrapperClassName?: string
+  iconWrapperClassName?: string
   /**
-   * 容器样式
+   * 前置图标容器样式
    */
-  wrapperStyle?: React.CSSProperties
-}
-
-const colorClassMap = {
-  'teal': 'bg-teal-100 text-teal-900',
-  'green': 'bg-green-100 text-green-900',
-  'sky': 'bg-sky-100 text-sky-900',
-  'blue': 'bg-blue-100 text-blue-900',
-  'gray': 'bg-gray-100 text-gray-900',
-  'red': 'bg-red-100 text-red-900',
-}
-
-const focusColorClassMap = {
-  'teal': 'hover:bg-teal-200 focus:ring-teal-300',
-  'green': 'hover:bg-green-200 focus:ring-green-300',
-  'sky': 'hover:bg-sky-200 focus:ring-sky-300',
-  'blue': 'hover:bg-blue-200 focus:ring-blue-300',
-  'gray': 'hover:bg-gray-200 focus:ring-gray-300',
-  'red': 'hover:bg-red-200 focus:ring-red-300',
-}
-
-const textColorClassMap = {
-  'teal': 'text-teal-500',
-  'green': 'text-green-500',
-  'sky': 'text-sky-500',
-  'blue': 'text-blue-500',
-  'gray': 'text-gray-500',
-  'red': 'text-red-500',
+  iconWrapperStyle?: React.CSSProperties
 }
 
 /**
@@ -79,25 +52,26 @@ export function Button(props: ButtonProps) {
     text,
     link,
     disabled,
-    wrapperClassName,
-    wrapperStyle,
+    iconWrapperClassName,
+    iconWrapperStyle,
     ...rest
   } = props
 
   return (
     <button
       className={classNames(
-        'disabled:cursor-not-allowed disabled:opacity-50 my-3',
+        'disabled:cursor-not-allowed disabled:opacity-50 my-2 h-10',
         {
-          'inline-flex justify-center items-center rounded-md px-4 py-2 shadow-sm transition duration-300 text-center':
-            !(text || link),
+          'rounded-md px-4 shadow-sm transition duration-300 text-center': !(
+            text || link
+          ),
           // 文本、链接模式无背景色和文字颜色加深
-          [colorClassMap[color]]: !(text || link),
+          [`bg-${color}-100 text-${color}-900`]: !(text || link),
           // 文本、链接模式在加载中、禁用时没有聚焦边框
-          [`focus:ring-2 ${focusColorClassMap[color]}`]:
+          [`focus:ring-2 hover:bg-${color}-200 focus:ring-${color}-300`]:
             !(text || link) && !(loading || disabled),
           // 文本、链接模式颜色
-          [textColorClassMap[color]]: text || link,
+          [`text-${color}-500`]: text || link,
           'w-full': block,
           'hover:underline': link,
         },
@@ -111,6 +85,7 @@ export function Button(props: ButtonProps) {
       ) : (
         <>
           <Transition
+            as="span"
             show={loading && !icon}
             enter="transition-all ease-out duration-300"
             enterFrom="transform-gpu opacity-0 scale-0 w-0"
@@ -118,21 +93,24 @@ export function Button(props: ButtonProps) {
             leave="transition-all ease-in duration-200"
             leaveFrom="transform-gpu opacity-100 scale-100 w-5"
             leaveTo="transform-gpu opacity-0 scale-0 w-0"
+            className="inline-block align-baseline"
           >
-            <Icon name="loading" className="animate-spin mr-1" />
+            <LoadingIcon />
           </Transition>
-          <div className={wrapperClassName} style={wrapperStyle}>
-            {icon && (
-              <span className={classNames({ 'mr-1': children })}>
-                {loading ? (
-                  <Icon name="loading" className="animate-spin" />
-                ) : (
-                  icon
-                )}
-              </span>
+          <span
+            className={classNames(
+              'inline-block align-baseline',
+              iconWrapperClassName
             )}
-            {children}
-          </div>
+            style={iconWrapperStyle}
+          >
+            {icon && (loading ? <LoadingIcon /> : icon)}
+          </span>
+          {typeof children === 'string' ? (
+            <span className="inline-block align-middle">{children}</span>
+          ) : (
+            children
+          )}
         </>
       )}
     </button>
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import './index.css'
+export * from './components/icon'
+export * from './components/button'
+export * from './components/dialog'
+export * from './components/input'
+export * from './components/textarea'
+export * from './components/form'
